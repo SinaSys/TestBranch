5e8956a807b12941ee265cbd90549573947a156c
SinaSystem <sinasystem47@gmail.com>
2020-06-09 02:21:13 +0430

    First commit

--- app/src/main/java/com/example/test12/MainActivity.kt
+++ app/src/main/java/com/example/test12/MainActivity.kt
@@ -9,4 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun methodFromLocal() {
+        println("Hello from local")
+    }
 }
